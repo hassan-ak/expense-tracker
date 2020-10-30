2be6038ce7a3b507624bb824cc929811f1f758ba
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 03:32:16 +0500

    Update AddTransaction Component
    Updated AddTransaction.js
    added section heading
    added Form for adding new transaction
    Form element consists of three divs one for description, second for amount and last for button
    In buttons div added two buttons one for income and second for expense
    
    Created and Updated AddTeansaction.css
    
    Updated App.js to create --error-color variable

--- src/components/addTransaction/AddTransaction.js
+++ src/components/addTransaction/AddTransaction.js
@@ -1,10 +1,58 @@
 // React Imports
-import React from 'react'
+import React from 'react';
+
+// Styles Import
+import './AddTransaction.css';
 
 export const AddTransaction = () => {
     return (
-        <div>
-            addTransaction
+        // Add Transaction Container
+        <div className="addTransaction">
+            
+            {/* Heading */}
+            <h3>
+                Add Transactions
+            </h3>
+
+            {/* Form for adding Transactions */}
+            <form>
+
+                {/* div for new Transaction Description */}
+                <div className="form-control">
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <input  type="text" 
+                            placeholder="Enter Description"/>
+                    <small>
+                        Kindly Enter Description
+                    </small>
+                </div>
+
+                {/* div for new Transaction Amount */}
+                <div className="form-control">
+                    <label htmlFor="amount">
+                        Amount
+                    </label>
+                    <input  type="number"
+                            step="0.00000000001" 
+                            placeholder="Enter Amount"/>
+                    <small>
+                        Kindly Enter Amount
+                    </small>
+                </div>
+
+                {/* Buttons for Adding new Transactions */}
+                <div className="btn">
+                    <button className="add-income"> 
+                        Add Income
+                    </button>
+                    <button className="add-expense">
+                        Add Expense
+                    </button>
+                </div>
+                
+            </form>
         </div>
     )
 }
