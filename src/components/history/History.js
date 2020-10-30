242342a725e2828e43f3923c02a79df57f9d3c34
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 02:58:11 +0500

    Update History Component
    Updated History.js to add ul for displaying transaction histrory while each entry is represented in form of li and added delete button with each transaction history
    
    Created and Updated History.css

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,10 +1,35 @@
 // React Imports
-import React from 'react'
+import React from 'react';
+
+// Styles Import
+import './History.css';
 
 export const History = () => {
     return (
-        <div>
-            History
+        <div className="transaction-history">
+            <h3>Transaction History</h3>
+            <ul className="list">
+                <li className="income-h">
+                    In
+                    <span>$100</span>
+                    <button className="del">X</button>
+                </li>
+                <li className="expense-h">
+                    Out
+                    <span>-$100</span>
+                    <button className="del">X</button>
+                </li>
+                <li className="income-h">
+                    In1
+                    <span>$250</span>
+                    <button className="del">X</button>
+        	    </li>
+                <li className="expense-h">
+                    Out1
+                    <span>-$70</span>
+                    <button className="del">X</button>
+                </li>
+            </ul>
         </div>
     )
 }
