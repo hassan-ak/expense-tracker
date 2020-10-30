6344a760689ec6e9d3147d9beeb4ed9bf55181db
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 02:35:01 +0500

    Update Balance Component
    Updated Balance.js
    added balance title and total amount
    Added and updated Balance.css

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,10 +1,17 @@
 // React Imports
 import React from 'react'
 
+// styles Import
+import './Balance.css'
+
 export const Balance = () => {
     return (
-        <div>
-            Balance
+        <div className="balance">
+            <h4>Balance</h4>
+            <h1>
+                <span>-</span>
+                <span>$100</span>
+            </h1>
         </div>
     )
 }
