813182d8c60ea07d223643a97be9ed2735300ee6
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 02:44:08 +0500

    Update Summary Component
    Updated Summary.js to add divs for income and expense
    
    Created and Updated Summary.css
    
    Updated App.css to define variables in roots.

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,10 +1,24 @@
 // React Imports
-import React from 'react'
+import React from 'react';
+
+// Styles Import
+import './Summary.css';
 
 export const Summary = () => {
     return (
-        <div>
-            Summary
+        <div className="income-expense">
+            <div>
+                <h4>Income</h4>
+                <p className="income-m">
+                    $ 0.00
+                </p>
+            </div>
+            <div>
+                <h4>Expense</h4>
+                <p className="expense-m">
+                    $ 0.00
+                </p>
+            </div>
         </div>
     )
 }
