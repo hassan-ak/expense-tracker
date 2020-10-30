77c8fb723c7a89179a6a2826f4575e3458671f05
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 02:29:43 +0500

    Update Header
    Added Content of Header.js (title of the app) and created and updated Header.css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
 // React Imports
 import React from 'react'
 
+// Styles Import
+import './Header.css'
+
 export const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header">
+            <h2>
+                Expense Tracker
+            </h2>
         </div>
     )
 }
