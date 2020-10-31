a9df218d7baffe2df2eee9898167e04f8b93d21b
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-31 15:32:48 +0500

    Update Balance.js
    Computed net balance by maping through the transaction amounts and adding them up. Based upon the net also determined sign of total.

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,10 +1,27 @@
 // React Imports
-import React from 'react';
+import React , { useContext } from 'react';
+
+// Functional Component Imports
+import { GlobalContext } from "../../functionalComponents/GlobalContext";
 
 // styles Import
 import './Balance.css';
 
 export const Balance = () => {
+
+    // use GloablContext to fetch data
+    const context = useContext(GlobalContext);
+
+    // Extarct amounts from data
+    const amounts = context.transactions.map(transaction => transaction.amount);
+
+    // Sum of Income Amounts
+    const total1 = amounts.reduce((acc,item)=>(acc+=item),0);
+    const total = total1.toFixed(2);
+
+    // Determine while total amount is +ve or -ve
+    const sign = total < 0 ?  '-' : '';
+
     return (
         // Balance Container
         <div className="balance">
@@ -14,8 +31,8 @@ export const Balance = () => {
 
             {/* Balance Amount */}
             <h1>
-                <span>-</span>
-                <span>$100</span>
+                <span>{sign}</span>
+                <span>${total}</span>
             </h1>
 
         </div>
